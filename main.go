9a3fdf336ae1ef6fc43062bccf349b7ef48ea002
Jonatan <jonatanav255@hotmail.com>
2024-03-20 08:31:39 -0600

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
 func main() {
-    var number float32 = 5.2
-    fmt.Println(number)
-    fmt.Println(int(number))
+	messagesFromDoris := []string{
+		"You doing anything later??",
+		"Did you get my last message?",
+		"Don't leave me hanging...",
+		"Please respond I'm lonely!",
+	}
+	numMessages := float64(len(messagesFromDoris))
+	costPerMessage := .02
+	
+	fmt.Println(numMessages)
+	// don't touch above this line
+
+	totalCost := costPerMessage * numMessages
+
+	// don't touch below this line
+
+	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
 }
